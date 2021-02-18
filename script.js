c07c60f70f725b76b756b3c2167e5e9c7831f734
Emil Lachinov <lachinov_em@mail.ru>
2021-02-18 23:41:23 +0500

    Задание 2

--- script.js
+++ script.js
@@ -1,31 +1,58 @@
-var searchParams = new URLSearchParams(window.location.search);
-var userName = searchParams.get('username');
-	if (searchParams.has('username') && userName !== '') {
+let loader = document.getElementById('loader');
+let load = function() {
+	loader.style.display = 'none';
+}
 
-fetch('https://api.github.com/users/'+userName)
+let url = window.location.toString();
+let arr = url.split('=');
+let name = arr[1];
+if (name == undefined)
+	name = 'EmilLachinov8';
+
+
+const getDate = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		load();
+		const dataDiv = document.createElement('div');
+		dataDiv.textContent = `Текущая дата: ${Date()}`;
+		document.body.append(dataDiv);
+		resolve();
+	}, 4000);
+});
+const getName = new Promise((resolve, reject) => {
+	setTimeout(() => name ? resolve(name) : reject('имя не найдено'), 2000);
+});
+
+Promise.all([getDate, getName])
+	.then(() => fetch(`https://api.github.com/users/${name}`))
 	.then(res => res.json())
 	.then(json => {
-		if (json.message == "Not Found") {
-			var div = document.createElement('div');
-			document.body.appendChild(div);
-			div.innerText = "Информация о пользователе не доступна";
+		console.log(json);
+
+		let img = document.createElement('img');
+		img.src = json.avatar_url;
+		img.alt = 'Avatar';
+		document.body.append(img);
+
+		let div = document.createElement('div');
+		document.body.append(div);
+
+		let userName = document.createElement('a');
+		if (json.name == null) {
+			userName.textContent = 'Имя пользователя недоступно.'
 		} else {
-
-			var h1 = document.createElement('h1');
-			document.body.appendChild(h1);
-			h1.innerHTML = json.name;
-
-			var div = document.createElement('div');
-			document.body.appendChild(div);
-			div.innerHTML = json.bio;
-
-			var a = document.createElement('a');
-			document.body.appendChild(a);
-		  a.innerHTML = 'API';
-			a.href = json.url;
-
-			var img = document.createElement('img');
-			document.body.appendChild(img);
-			img.src = json.avatar_url;
+			userName.innerHTML = json.name;
+			userName.href = json.html_url;
+			userName.classList.add('link');
 		}
-}
+		div.append(name);
+
+		let bio = document.createElement('p');
+		if (json.bio == null) {
+			bio.textContent = 'Информация о пользователе недоступна.'
+		} else {
+			bio.textContent = json.bio;
+		}
+		div.append(bio);
+	})
+	.catch(err => console.log(err));
